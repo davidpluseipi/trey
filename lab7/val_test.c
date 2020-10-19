2ac40941d9fa22d3c1206e9c606ee83d01a8821a
David Meissner <davidpluseipi@gmail.com>
2020-10-19 15:49:21 -0700

    mods to val_test.c in lab7

--- lab7/val_test.c
+++ lab7/val_test.c
@@ -2,10 +2,10 @@
 
 void f(void) {
   int* x = malloc(10*sizeof(int));
-  x[10] = 0; // heap block overrun
-} // memory leak; x is not freed
+  x[10] = 0; // problem 1: heap block overrun
+} //end f    // problem 2: memory leak; x is not freed
 
 int main(void) {
   f();
   return 0;
-}
+} // end main
